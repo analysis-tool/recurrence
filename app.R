97777edef5db1a71e0370f08879b2a021e012dcf
recurrencerisk <35275354+recurrencerisk@users.noreply.github.com>
2019-06-04 08:59:22 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -125,10 +125,10 @@ ui <- fluidPage(
                                      br(),
                                      HTML("<b>Input</b>
                                           <ul><li> <b>CSV Data File</b>: the case-listing unformatted CSV data file exported from SEER*Stat software with modification of variables of interest or user-generated individual data by other statistical software in the CSV format. Note that, 
-                                                                         users may not directly use the original unformatted variables, but need to modify the case-listing SEER*Stat data before uploading because 1) it may not contain the event variable for death due to cancer; 
+                                                                         users may not directly use the original unformatted variables, but need to modify the case-listing SEER*Stat data before uploading because 
+                                                                         1) it may not contain the event variable for death due to cancer and the dead events due to other causes should be recoded as censoring events; 
                                                                          2) there may not be categorical but continuous variables for strata and covariates; 
-                                                                         3) the stage/strata/covariates variables are categorical but with many levels or with numerical unknown values, the user will need to recode it to better categorize the group;
-                                                                         4) the dead events due to other causes should be recoded as censoring events.
+                                                                         3) the stage/strata/covariates variables are categorical but with many levels or with numerical unknown values, the user will need to recode it to better categorize the group.
                                           </li><li> <b>Strata</b>: the user-defined strata variables which should be categorical coded as integers.
                                           </li><li> <b>Covariates</b>: the user-defined covariates in the mixture cure survival model which should be categorical coded as integers.
                                           </li><li> <b>Time Variable</b>: the variable specified as follow-up time in the survival model.
